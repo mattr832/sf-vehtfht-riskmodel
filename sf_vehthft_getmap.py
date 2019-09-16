f1b13936cbd3227a0d383d4240444bf6d19d2de1
mattr832 <mattr832@gmail.com>
2019-09-15 22:54:19 -0700

    test to confirm correct mapping

--- sf_vehthft_getmap.py
+++ sf_vehthft_getmap.py
@@ -9,11 +9,13 @@ def get_map(response):
     folmap = folium.Map([float(response['Latitude']), float(response['Longitude'])], zoom_start=16, width='90%', height='90%')
     folium.Marker(
             location=[float(response['Latitude']), float(response['Longitude'])],
-            popup=folium.Popup(html="Risk Score: %s <br> Risk Level: %s <br> Risk Score in 3 Hours: %s <br> Risk Level in 3 Hours: %s" 
+            popup=folium.Popup(html="Risk Score: %s <br> Risk Level: %s <br> Risk Score in 3 Hours: %s <br> Risk Level in 3 Hours: %s <br> Lat: %s <br> Long: %s" 
                                %(response['Risk Score'],
                                  response['Risk Level'],
                                  response['3+ Hour Risk Score'],
-                                 response['3+ Hour Risk Level']),
+                                 response['3+ Hour Risk Level'],
+                                 response['Latitude'],
+                                 response['Longitude']),
                               max_width=500),
             icon=folium.Icon(color='green')
     ).add_to(folmap)
